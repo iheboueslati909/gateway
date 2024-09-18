07f540f0ec734c5ac90913189b15740a5b871936
iheboueslati909 <hooba500@gmail.com>
2024-09-18 22:28:43 +0100

    url path translated to MS's cmd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -23,11 +23,11 @@ export class AppController {
   @Get('*')
   async handleRequest(@Req() req: Request, @Res() res: Response) {
     const service = req.path.split('/')[1];
-
     const client = this.routeRequest(service);
+    const command = `${req.method.toUpperCase()}/${service.toUpperCase()}/${req.path.split('/').slice(2).join('/').toUpperCase()}`;
 
     try {
-      const response = await client.send({ cmd: req.method }, req.body).toPromise();
+      const response = await client.send({ cmd: command }, req.body).toPromise();
       return res.json(response);
     } catch (error) {
       return res.status(500).json({ message: 'Error processing request', error: error.message });
